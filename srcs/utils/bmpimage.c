0cd1cca39d1a1e895b930a8fedd48c924ffaaa22
lbertran <lbertran@student.42lyon.fr>
2021-02-13 15:18:48 +0100

    norm

--- srcs/utils/bmpimage.c
+++ srcs/utils/bmpimage.c
@@ -6,7 +6,7 @@
 /*   By: lbertran <lbertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 12:46:29 by lbertran          #+#    #+#             */
-/*   Updated: 2021/02/13 15:05:33 by lbertran         ###   ########lyon.fr   */
+/*   Updated: 2021/02/13 15:18:35 by lbertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,11 +66,11 @@ static int	mir_verti(t_view *view)
 
 static int	bitmap_info_header(t_view *view, int fd)
 {
-	int 	header_info_size;
-	int 	plane_nbr;
-	int 	o_count;
+	int		header_info_size;
+	int		plane_nbr;
+	int		o_count;
 
-	header_info_size = 40;
+	header_info_size = INFO_HEADER_SIZE;
 	plane_nbr = 1;
 	write(fd, &header_info_size, 4);
 	write(fd, &view->settings->width, 4);
@@ -88,16 +88,16 @@ static int	bitmap_info_header(t_view *view, int fd)
 
 int			create_bitmap(t_view *view)
 {
-	int		 fd;
-	int 	file_size;
-	int 	first_pix;
+	int		fd;
+	int		file_size;
+	int		first_pix;
 	char	*name;
 
 	name = ft_strdup("image.bmp");
 	fd = open(name, O_CREAT | O_RDWR, 0777);
-	file_size = 14 + 40 + 4 + (view->settings->width *
-		view->settings->height) * 4;
-	first_pix = 14 + 40 + 4;
+	file_size = FILE_HEADER_SIZE + INFO_HEADER_SIZE + 4 +
+		(view->settings->width * view->settings->height) * 4;
+	first_pix = FILE_HEADER_SIZE + INFO_HEADER_SIZE + 4;
 	write(fd, "BM", 2);
 	write(fd, &file_size, 4);
 	write(fd, "\0\0\0\0", 4);
