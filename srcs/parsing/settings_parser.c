e28f0597b417c8c87aba184abb250394d162359c
lbertran <lbertran@student.42lyon.fr>
2021-03-01 14:59:14 +0100

    fix duplicate config entry

--- srcs/parsing/settings_parser.c
+++ srcs/parsing/settings_parser.c
@@ -6,7 +6,7 @@
 /*   By: lbertran <lbertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 16:06:08 by lbertran          #+#    #+#             */
-/*   Updated: 2021/02/26 16:14:02 by lbertran         ###   ########lyon.fr   */
+/*   Updated: 2021/03/01 14:58:31 by lbertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ int	parse_resolution(char **split, t_settings *settings)
 		free_split(split);
 		return (print_error("Invalid resolution in .cub file."));
 	}
+	if (settings->width != 0 || settings->height != 0)
+		print_error_exit("Duplicate resolution in .cub file.", 1);
 	settings->width = ft_atoi(split[1]);
 	if (settings->width > 2559)
 		settings->width = 2559;
@@ -43,6 +45,8 @@ int	parse_resolution(char **split, t_settings *settings)
 
 int	parse_color_sky(t_settings *settings, char **colorsplit)
 {
+	if (settings->sky_color != -1)
+			print_error_exit("Duplicate sky color in .cub file.", 1);
 	settings->sky_color =
 		rgbint(ft_atoi(colorsplit[0]), ft_atoi(colorsplit[1]),
 			ft_atoi(colorsplit[2]));
@@ -65,6 +69,8 @@ int	parse_color(char **split, t_settings *settings, int ground)
 	}
 	if (ground)
 	{
+		if (settings->ground_color != -1)
+			print_error_exit("Duplicate ground color in .cub file.", 1);
 		settings->ground_color =
 		rgbint(ft_atoi(colorsplit[0]), ft_atoi(colorsplit[1]),
 			ft_atoi(colorsplit[2]));
